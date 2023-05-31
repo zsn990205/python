beb5a2caee0ceb30f0dd1a6eb3f799e043305a73
朱思楠 <zsn990205@163.com>
2023-05-31 11:31:50 +0800

    提交

---
+++ pythonProject/code04.py
@@ -0,0 +1,25 @@
+
+
+a = 10
+print(type(a))
+
+b = 0.5
+print(type(b))
+
+c = 'hello'
+print(type(c))
+
+d = '''my 'name' is "zsn"'''
+print(d)
+
+print(len(c))
+
+a1 = 'hello'
+a2 = 'world'
+b2 = 10
+print(a1 + a2)
+
+c1 = True
+c2 = False
+print(type(c1))
+print(type(c2))

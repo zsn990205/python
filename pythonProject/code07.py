1e11cf0175baf05914c13b81e7278dd765aa9eb3
朱思楠 <zsn990205@163.com>
2023-06-03 12:02:34 +0800

    提交代码

---
+++ pythonProject/code07.py
@@ -0,0 +1,12 @@
+
+
+#通过控制台来输入一个数据
+# num = input('请输入一个整数')
+# print(f'您输入的数字是{num}')
+# print(type(num))
+
+a = input('请输入第一个整数')
+b = input('请输入第二个整数')
+a = int(a)
+b = int(b)
+print(f'a + b = {a + b}')

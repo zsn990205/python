addcb1632294003498fa9705eea1542586525107
朱思楠 <zsn990205@163.com>
2023-06-12 16:57:24 +0800

    提交代码

---
+++ pythonProject/code15.py
@@ -0,0 +1,14 @@
+
+
+# a = input('请输入一个整数: ')
+# if a == '1':
+#    print('aaa')
+#    print('bbb')
+
+a = input('请输入第一个整数: ')
+b = input('请输入第二个整数: ')
+if a == '1':
+    if b == '2':
+        print('aaa')
+    print('bbb')
+print('ccc')

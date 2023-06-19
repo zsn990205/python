06de3a64c305a30d33e4d8276259b9597b17bf8a
朱思楠 <zsn990205@163.com>
2023-06-19 18:24:48 +0800

    提价代码

---
+++ pythonProject/code22.py
@@ -0,0 +1,22 @@
+#以下的代码的类型相同,我们可以使用一个函数将其封装起来
+
+#1.求1-100的和
+theSum = 0
+for i in range(1,101):
+
+    theSum += i
+print(theSum)
+
+#2.求300-400的和
+theSum = 0
+for i in range(300,401):
+    theSum += i
+
+print(theSum)
+
+#3.求1-1000的和
+theSum = 0
+for i in range(1,1001):
+    theSum += i
+
+print(theSum)
